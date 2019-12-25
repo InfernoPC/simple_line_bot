7c3d547743740bb86e521088271b08ef38770055
Tim Chen <inferno6562@gmail.com>
2019-12-25 11:13:37 +0800

    first deploy

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+
+	protect_from_forgery with: :null_session
+	def webhook
+
+		client = Line::Bot::Client.new {|config|
+			config.channel_secret = ENV['line_channel_secret']
+			config.channel_token = ENV['line_channel_token']
+		}
+
+		reply_token = params['events'].first['replyToken']
+
+		response_message = {
+			type: 'text',
+			text: params['events'].first['message']['text']
+		}
+
+		client.reply_message reply_token, response_message
+
+		head: ok
+	end
+end
