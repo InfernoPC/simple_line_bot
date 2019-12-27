19be454a0c110b8ead24a6d365bbccf823a5efa3
Tim Chen <inferno6562@gmail.com>
2019-12-27 17:35:32 +0800

    test some actions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,13 +10,79 @@ class WelcomeController < ApplicationController
 
 		reply_token = params['events'].first['replyToken']
 
-		response_message = {
-			type: 'text',
-			text: params['events'].first['message']['text']
-		}
+		response_message = case params['events'].first['message']['text']
+		when 'pb'
+			reply_pb
+		when 'msg'
+			reply_msg
+		when 'uri'
+			reply_uri
+		when 'dt'
+			reply_dt
+		when 'cam'
+			reply_cam
+		when 'loc'
+			reply_loc
+		when '?'
+			{
+				type: 'text',
+				text: 'pb, msg, uri, dt, cam, loc'
+			}
+		else
+			{	
+				type: 'text',
+				text: params['events'].first['message']['text']
+			}
+		end
+
 
 		client.reply_message reply_token, response_message
 
 		head :ok
 	end
+
+	private
+	def reply_pb
+		{
+			type: 'postback',
+			label: 'postback',
+			data: 'action=postback',
+			displayText: 'try postback'
+		}
+	end
+	def reply_msg
+		{
+			type: 'message',
+			label: 'reply',
+			text: 'message sent!'
+		}
+	end
+	def reply_uri
+		{
+			type: 'uri',
+			label: 'google.com',
+			uri: 'http://www.google.com/'
+		}
+	end
+	def reply_dt
+		{
+			type: 'datetimepicker',
+			label: 'select date',
+			date: 'storeId=12345', # ???
+			mode: 'datetime',
+		}
+	end
+	def reply_cam
+		{
+			type: 'camera',
+			label: 'Camera'
+		}
+	end
+	def reply_loc
+		{
+			type: 'location',
+			label: 'Location'
+		}
+	end
+
 end
