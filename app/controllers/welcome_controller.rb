b954e1a4828adf919915fa4b32b9ec996c9e50a8
Tim Chen <inferno6562@gmail.com>
2019-12-30 16:28:15 +0800

    add rock paper scissors

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
 
+	DRAW_MESSAGE = ['哈哈～平手！', '好阿，學我出一樣的。']
+	WIN_MESSAGE = ['我贏啦～哼哼！', '歐耶～我贏了！']
+	LOSE_MESSAGE = ['你贏了，呿。', '好阿都給你贏就好啦']
+
 	protect_from_forgery with: :null_session
 	def webhook
 
@@ -17,7 +21,7 @@ class WelcomeController < ApplicationController
 				text: 'Select one.',
 				quickReply: {
 					items: [
-						{	type: 'action', action: { type: 'message', label: 'sushi', text: 'sushi' }},
+						{	type: 'action', action: { type: 'message', label: '猜拳', text: '猜拳' }},
 						{	type: 'action', action: { type: 'message', label: 'tempura', text: 'tempura' }},
 						{	type: 'action', action: { type: 'location', label: 'send location' }}
 					]
@@ -28,6 +32,43 @@ class WelcomeController < ApplicationController
 				type: 'text',
 				text: 'menu'
 			}
+		when '猜拳'
+			{
+				type: 'text',
+				text: '剪刀，石頭…',
+				quickReply: {
+					items: [
+						{type: 'action', action: {
+							type: 'message', label: '剪刀', text: '剪刀'
+						}}, 
+						{type: 'action', action: {
+							type: 'message', label: '石頭', text: '石頭'
+						}}, 
+						{type: 'action', action: {
+							type: 'message', label: '布', text: '布'
+						}} 
+					]
+				}
+			}
+		when '剪刀', '石頭', '布'
+			user = params['events'].first['message']['text']
+			ai = ['剪刀', '石頭', '布'].sample
+			if user == ai
+				{
+					type: 'text', 
+					text: "#{ai}！ #{DRAW_MESSAGE.sample}"
+				}
+			elsif (user == '剪刀' and ai == '石頭') or (user == '石頭' and ai == '布') or (user == '布' and ai == '剪刀')
+				{
+					type: 'text',
+					text: "#{ai}！ #{WIN_MESSAGE.sample}"
+				}
+			else
+				{
+					type: 'text',
+					text: "#{ai}！ #{LOSE_MESSAGE.sample}"
+				}
+			end
 		else
 			{	
 				type: 'text',
